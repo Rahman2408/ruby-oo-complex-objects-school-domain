97b5805543a25335704cb2757c0b76502fc7f8ca
Abdelrahman Elsirafy <rahman2408@gmail.com>
2021-01-23 03:07:34 -0500

    commit tree c4773471b881468769111400ecfcf103d2f28617

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
 # code here!
+require 'pry'
+class School
+    
+    attr_accessor :roster
+    def initialize(name)
+        @name=name
+        @roster={}
+        
+    end
+
+    def add_student(name, grade)
+        if @roster.has_key? grade 
+            @roster[grade] << name
+        else
+           @roster[grade]=[name]
+        end
+    end
+
+    def grade(key)
+        @roster[key]
+    end
+    
+    def sort
+       @sorted_roster={}
+       @roster.keys.each do|sorted_keys|
+        @sorted_roster[sorted_keys]= @roster[sorted_keys].sort
+       end
+       @sorted_roster
+    end
+ # @@roster=[:"#{student_name}"]<< grade
+end
+
+
